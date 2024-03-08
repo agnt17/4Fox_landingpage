627e1c7974b2a3a87b08b379c439de231304a6ea
frozenopertor <adityagupta11112002@gmail.com>
2024-03-08 21:17:43 +0530

    yarn build is done

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -23,27 +23,25 @@ const Hero = () => {
       <div className="lg:hidden flex mt-8 mx-auto w-4/6">
         <img src="./fox-logo.png" alt="Fox Logo" />
       </div>
-      <div className="max-w-[800px] mt-[-96px] w-full h-screen text-center flex flex-col justify-center">
-        <div className="">
-          <h1 className="w-full md:text-8xl sm:text-5xl text-3xl font-bold select-none">
-            BUISNESS SOLUTIONS
-          </h1>
-          <div className="flex justify-center items-center pr-3">
-            <p className="md:text-3xl sm:text-2xl text-l font-bold py-4">
-              That Truly Inspires Growth
-            </p>
-          </div>
-          <p className="md:text-2xl text-xl font-bold text-gray-500">
-            Manage your bookings easier and make your life easier.
+      <div className="max-w-[800px] mt-[-96px] w-full h-screen text-center flex flex-col justify-center items-center mx-auto">
+        <h1 className="w-full md:text-8xl sm:text-5xl text-3xl font-bold select-none">
+          BUISNESS SOLUTIONS
+        </h1>
+        <div className="flex justify-center items-center pr-3">
+          <p className="md:text-3xl sm:text-2xl text-l font-bold py-4">
+            That Truly Inspires Growth
           </p>
-          <div className="flex lg:flex-row flex-col">
-            <button className="bg-orange-600 w-[210px] text-xl mt-[3rem] lg:mr-10 rounded-md font-medium my-6 mx-auto py-3 text-black hover:scale-105 duration-300 ease-in-out">
-              Services
-            </button>
-            <button className="bg-orange-600 w-[210px] text-xl mt-[3rem] rounded-md font-medium my-6 mx-auto py-3 text-black hover:scale-105 duration-300 ease-in-out">
-              Brief
-            </button>
-          </div>
+        </div>
+        <p className="md:text-2xl text-xl font-bold text-gray-500">
+          Manage your bookings easier and make your life easier.
+        </p>
+        <div className="flex lg:flex-row flex-col">
+          <button className="bg-orange-600 w-[210px] text-xl mt-[3rem] lg:mr-10 rounded-md font-medium my-6 mx-auto py-3 text-black hover:scale-105 duration-300 ease-in-out">
+            Services
+          </button>
+          <button className="bg-orange-600 w-[210px] text-xl mt-[3rem] rounded-md font-medium my-6 mx-auto py-3 text-black hover:scale-105 duration-300 ease-in-out">
+            Brief
+          </button>
         </div>
       </div>
     </>
